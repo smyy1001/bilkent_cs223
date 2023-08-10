2b24fc75ef08dae456c64d52982f30206a6b1638
sumeyye acar <99368893+smyy1001@users.noreply.github.com>
2023-08-10 16:18:26 +0300

    Create clock_divider.sv

---
+++ lab5/clock_divider.sv
@@ -0,0 +1,21 @@
+// Clock Divider
+module ClockDiv( input logic clk, output logic slow );
+    
+    // Clock Counter
+    logic [28:0] counter;
+
+    always @(  posedge clk  )
+        begin  
+            if(  counter >= 49999999  )
+                begin
+                    counter <= 0;
+                    slow    <= 0;
+                end
+            else
+                begin
+                    counter <= counter + 1;
+                    slow    <=           1;
+                end
+        end
+
+endmodule
